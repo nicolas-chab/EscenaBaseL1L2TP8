49197c8fe8fa21dd70c09f17ffa326bc2a933c97
nicolas-chab <47203227@est.ort.edu.ar>
2022-09-09 20:38:09 -0300

    pickup done, finish putting the remaining spawnpoints

---
+++ Assets/Scripts/Randomspawnpoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomspawnpoint : MonoBehaviour
+{
+    //create an array of spawn points, assigned in inspector 
+    public Transform[] collectableSpawnPoints = new Transform[2];
+
+    //create an array of collectables to choose from
+    public GameObject[] items = new GameObject[4];
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnCollectables();
+    }
+
+   
+
+    //selects spawn point
+    public Transform GetCollectableSpawnPoint()
+    {
+        //randomly selects a point out of the array
+        int index = Random.Range(0, collectableSpawnPoints.Length);
+        //returns the selected point
+        return collectableSpawnPoints[index];
+    }
+
+    //selects object to spawn
+    public GameObject GetCollectable()
+    {
+        //selects one of the items from the array
+        int index = Random.Range(0, items.Length);
+        //returns the object selected
+        return items[index];
+    }
+
+    // spawns the random object on the random point
+    public GameObject SpawnCollectables()
+    {
+        //selects the spawn point
+        Transform spawnPoint = GetCollectableSpawnPoint();
+        //selects the object
+        GameObject collectable = GetCollectable();
+        //creates the object selected on the point selected
+        GameObject c = Instantiate(collectable, spawnPoint.position, spawnPoint.rotation) as GameObject;
+        //spawns the object
+        return c;
+    }
+}
---
+++ Assets/Scripts/pickupobject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupobject : MonoBehaviour
+{
+    public Transform destination;
+    private void OnMouseDown()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = destination.position;
+        this.transform.parent = GameObject.Find("destinationofobj").transform;
+    }
+    void OnMouseUp()
+    {
+        GetComponent<BoxCollider>().enabled = true;
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
